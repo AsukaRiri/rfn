70777c8628a811c50bc09d16eace56ec1c816745
AsukaRiri <raymond55chow55@gmail.com>
2017-02-23 19:02:23 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,18 +4,23 @@ routes.menu = ['#menu'];
 routes.exclude = '.langBtn';
 
 routes.callback = function() {
+  // Fire when hash change, after page loaded
 }
-
-route('/', 'home.html', function () {});
+route('/', 'home.html', function () {
+  // Fire in specific page, after page loaded
+});
 
 $(document).ready(function() {
   if (rfn.config.browser.versions.mobile) {
-    // iPhone or Android Phone but not iPad
+    // iPhone, iPad, Android, Non-desktop Device
+  }
+  if (rfn.config.browser.versions.iPhone) {
+    // iPhone
   }
   if (rfn.config.browser.versions.iPad) {
     // iPad
   }
-  if (rfn.config.browser.versions.mobile || rfn.config.browser.versions.android || rfn.config.browser.versions.iPad) {
-    // all non desktop device 
+  if (rfn.config.browser.versions.android) {
+    // Android Device
   }
 });
